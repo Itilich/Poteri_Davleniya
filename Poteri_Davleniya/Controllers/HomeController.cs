c2301d02ff1a6dd879a45664caaa5739f8f44639
Itilich <aaperlovka@gmail.com>
2025-05-21 16:22:27 +0500

    2nd step

--- Poteri_Davleniya/Controllers/HomeController.cs
+++ Poteri_Davleniya/Controllers/HomeController.cs
@@ -18,9 +18,79 @@ namespace Poteri_Davleniya.Controllers
 
         public IActionResult Index()
         {
-            var variants = _context.Variants.FirstOrDefault();
-            return View();
+            var variants = _context.Variants.ToList();
+            return View(variants);
         }
+
+        public IActionResult Delete(int id)
+        {
+            var variant = _context.Variants.FirstOrDefault(x => x.ID == id);
+
+            if (variant != null)
+            { 
+                _context.Variants.Remove(variant);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Calc(int id)
+        {
+            var variant = _context.Variants.FirstOrDefault(x => x.ID == id);
+            var viewModel = new HomeCalcViewModel();
+
+            if (variant != null) 
+            {
+                viewModel.pipe_lenght = variant.pipe_lenght;
+                viewModel.pipe_quantity = variant.pipe_quantity;
+                viewModel.rows_along = variant.rows_along;
+                viewModel.rows_across = variant.rows_across;
+                viewModel.step_along = variant.step_along;
+                viewModel.step_across = variant.step_across;
+                viewModel.pipe_diameter = variant.pipe_diameter;
+                viewModel.pipe_thickness = variant.pipe_thickness;
+                viewModel.smoke_temp = variant.smoke_temp;
+                viewModel.heating_air_temp = variant.heating_air_temp;
+                viewModel.air_temp = variant.air_temp;
+                viewModel.heating_air_outgo = variant.heating_air_outgo;
+                viewModel.space_air_pass = variant.space_air_pass;
+                viewModel.smoke_outgo = variant.smoke_outgo;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Calc(CalcModel model)
+        {
+
+            var viewModel = new HomeCalcViewModel()
+            {
+            };
+
+            _context.Variants.Add(new Variant
+            {
+                pipe_lenght = model.pipe_lenght,
+                pipe_quantity = model.pipe_quantity,
+                rows_along = model.rows_along,
+                rows_across = model.rows_across,
+                step_along = model.step_along,
+                step_across = model.step_across,
+                pipe_diameter = model.pipe_diameter,
+                pipe_thickness = model.pipe_thickness,
+                smoke_temp = model.smoke_temp,
+                heating_air_temp = model.heating_air_temp,
+                air_temp = model.air_temp,
+                heating_air_outgo = model.heating_air_outgo,
+                space_air_pass = model.space_air_pass,
+                smoke_outgo = model.smoke_outgo,
+            });
+            _context.SaveChanges();
+
+            return View(viewModel);
+        }
+
 
         public IActionResult Privacy()
         {
---
+++ Poteri_Davleniya/Models/HomeCalcViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Poteri_Davleniya.Models
+{
+    public class HomeCalcViewModel
+    {
+        public double? pipe_lenght { get; set; }
+        public int? pipe_quantity { get; set; }
+        public int? rows_along { get; set; }
+        public int? rows_across { get; set; }
+        public double? step_along { get; set; }
+        public double? step_across { get; set; }
+        public double? pipe_diameter { get; set; }
+        public double? pipe_thickness { get; set; }
+        public double? smoke_temp { get; set; }
+        public double? heating_air_temp { get; set; }
+        public double? air_temp { get; set; }
+        public double? heating_air_outgo { get; set; }
+        public double? space_air_pass { get; set; }
+        public double? smoke_outgo { get; set; }
+    }
+}
